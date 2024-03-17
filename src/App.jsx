a0179d554d7c163ee19a347be71e373ebea40c98
upadhyayaditi <au.aditi08@gmail.com>
2024-03-18 02:31:23 +0530

    benefits and heading components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import ButtonGradient from './assets/svg/ButtonGradient';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 
-import Header from './components/Header';
-import Hero from './components/Hero';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return(
+  return (
     <>
-      
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
